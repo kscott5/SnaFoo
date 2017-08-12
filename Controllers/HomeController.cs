374c99c61b1a3cf2c761ea6e4f500baf3f3438b6
Karega Scott <kkscott134@gmail.com>
2017-08-11 20:58:25 -0500

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace nerdy.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string PAGE_TITLE = "SNA Foo - %0";
+
+        private void SetViewDataTitle(string title) {
+            ViewData["Body_Title"] = "Voting";
+            ViewData["Title"] = string.Format(PAGE_TITLE, ViewData["Body_Title"]);            
+        }
+
+        [Route("Voting")]
+        public IActionResult SnaFoo3()
+        {
+            this.SetViewDataTitle("Voting");
+            return View();
+        }
+
+        [Route("ShoppingList")]
+        public IActionResult SnaFoo2()
+        {
+            this.SetViewDataTitle("Shopping List");
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [Route("Suggestions")]
+        public IActionResult SnaFoo1()
+        {
+            this.SetViewDataTitle("Shopping List");
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
