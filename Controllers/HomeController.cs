79f4e4fca0796989fe2c3bbe05f70f695da33360
Karega Scott <kkscott134@gmail.com>
2017-08-12 11:56:26 -0500

    Configuring basic Logging and Form HttpPOST

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,21 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace nerdy.Controllers
 {
+    [Route("Home")]
     public class HomeController : Controller
     {
+        private ILogger logger;
+
+
+        public HomeController(ILoggerFactory loggerFactory) : base() {
+            this.logger = loggerFactory.CreateLogger("HomeController");
+        }
         private const string PAGE_TITLE = "SNA Foo - %0";
 
         private void SetViewDataTitle(string title) {
-            ViewData["Body_Title"] = "Voting";
-            ViewData["Title"] = string.Format(PAGE_TITLE, ViewData["Body_Title"]);            
+            ViewData["Body_Title"] = title;
+            ViewData["Title"] = string.Format(PAGE_TITLE, title);
+        }
+
+        [HttpPost]
+        [Route("Suggestions")]
+        public void SnaFoo4([FromForm] string suggestionsInput, [FromForm] string suggestionLocation  ) {
+            
         }
 
         [Route("Voting")]
         public IActionResult SnaFoo3()
         {
+            this.logger.LogDebug("HttpGet: Voting");
+
             this.SetViewDataTitle("Voting");
             return View();
         }
@@ -26,20 +42,17 @@ namespace nerdy.Controllers
         public IActionResult SnaFoo2()
         {
             this.SetViewDataTitle("Shopping List");
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
         [Route("Suggestions")]
         public IActionResult SnaFoo1()
         {
-            this.SetViewDataTitle("Shopping List");
-            ViewData["Message"] = "Your contact page.";
-
+            this.SetViewDataTitle("Suggestions");
             return View();
         }
 
+        [Route("Error")]
         public IActionResult Error()
         {
             return View();
