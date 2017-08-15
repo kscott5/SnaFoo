c4860350d2c10667cc3b60ef716a0bb924fff86d
Karega K. Scott <kkscott@outlook.com>
2017-08-15 13:03:29 -0500

    Implementation based on expected API response

--- Services/SnaAPIService.cs
+++ Services/SnaAPIService.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
+using Newtonsoft.Json;
+
+using System.Threading.Tasks;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using Microsoft.Extensions.Configuration;
 
 using nerdy.Models;
 
@@ -17,28 +20,72 @@ namespace nerdy.Services {
     /// JavaScript developers.
     /// </summary>
     public class SnaAPIService : SnackService {
-        private string API_KEY = "some api key";
-        private string API_URL = "some api url";
-       
+        private string api_auth_key = "some api key";
+        private string api_base_url = "some api url";
+              
         public SnaAPIService(ILoggerFactory loggerFactory, 
             IConfigurationRoot configurationRoot, VotingService votingService) : 
             base(loggerFactory, configurationRoot, votingService) {
             this.Logger = loggerFactory.CreateLogger("Nerdy.Services.SnaAPI");
             this.inMemorySnacks = new List<Snack>();
 
-            this.API_KEY = this.Configuration.GetValue("NerdyApi/Key", API_KEY);
-            this.API_URL = this.Configuration.GetValue("NerdyApi/Url", API_URL);
+            this.api_auth_key = this.Configuration.GetValue("NerdyApi:Key", api_auth_key);
+            this.api_base_url = this.Configuration.GetValue("NerdyApi:Url", api_base_url);
 
-            this.Logger.LogDebug("API Key: " + API_KEY);
-            this.Logger.LogDebug("API Url: " + API_URL);
+            this.Logger.LogDebug("API Key: " + api_auth_key);
+            this.Logger.LogDebug("API Url: " + api_base_url);
         }
 
+        /// <summary>
+        /// Retrieves a list of snacks from the Nerdy Snack API
+        /// </summary>
+        /// <return> List of Snacks</return>
         public override IList<Snack> GetSnacks() {
-            throw new NotImplementedException();
-        }
+            var url = string.Format("{0}/snack", this.api_base_url);
 
+            this.Logger.LogDebug("Get Snacks: TODO");
+            this.Logger.LogDebug("HttpGet: {0} - Check Response Codes", url);
+            
+            IList<Snack> snacks = new List<Snack>();
+
+            try {
+            WebClient client = new WebClient();
+            client.Headers.Add(HttpRequestHeader.Authorization, this.api_auth_key);
+            var jsonData = client.DownloadString(api_base_url);
+                       
+            snacks = JsonConvert.DeserializeObject<List<Snack>>(jsonData);
+            } catch(WebException) {
+                // Do something
+            }
+
+            return snacks;
+         }
+        
+        /// <summary>
+        /// Sends a snack to the Nerdy Snack API and local <cref name="VotingService"/>
+        /// cache.
+        /// </summary>
+        /// <return> True if the saved success, else False</return>    
         public override Boolean SaveSnack(Snack data){
-            throw new NotImplementedException();
+            var url = string.Format("{0}/snacks", this.api_base_url);
+
+            this.Logger.LogDebug("Save Snacks: TODO");
+            this.Logger.LogDebug("HttpPost: {0} - Check Response Codes", url);
+
+            try {
+                WebClient client = new WebClient();
+                client.Headers.Add(HttpRequestHeader.Authorization, this.api_auth_key);
+                var jsonData = client.DownloadString(api_base_url);
+            
+                var newSnack = JsonConvert.DeserializeObject<Snack>(jsonData);
+                this.VotingService.SaveSnack(newSnack);
+
+                return true;
+            } catch(WebException) {
+                // Do something
+            }
+
+            return false;
         }
     }
 }
